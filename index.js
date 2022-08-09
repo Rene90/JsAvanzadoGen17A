fc2947d95b83e78541b378774d7fcee407fe88cb
Rene90 <rob_rene199027@hotmail.com>
2022-08-08 20:25:44 -0600

    1 clase de node

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Estoy escribiendo codigo desde la terminal")
+var colors = require("colors")
+
+function Sumar(x,y){
+	let suma = x+y
+	console.log("La suma es:".red +suma)
+
+}
+
+
+Sumar(30,40)
+
+
+for (let i=0; i<5;i++){
+
+	console.log("Mabel".magenta)
+
+}
