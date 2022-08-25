194c27178bc6a5299e75cb1015b7484ec7da7148
Jesua Luján <code.masterqro@gmail.com>
2022-08-24 20:03:23 -0500

    ejecutamos a createAuthor para crear un nuevo registro

--- 06ApiCrud/main.js
+++ 06ApiCrud/main.js
@@ -7,4 +7,17 @@ const goodReadsCrud = require('./crudAuthors')
 // goodReadsCrud.listAuthors()
 
 //*LISTA UN AUTOR POR SU ID
-goodReadsCrud.getAuthor(14639)
+// goodReadsCrud.getAuthor(14814)  //14639 //14814 //14825
+
+//*CREAR UN AUTOR 
+const jsonSend = {
+        name: "CRISTIANO",
+        last_name: "RONALDO",
+        nacionalidad: "MX",
+        biography: "FUTBOLISTA PROFESIONAL",
+        gender: "M",
+        age: 35,
+        is_alive: true
+    }
+goodReadsCrud.createAuthor(jsonSend)
+
