b52b786ba8a8de361295961455d85d52df387e1d
Jesua Luján <code.masterqro@gmail.com>
2022-08-24 19:21:16 -0500

    mando a llamar y ejecuto a listAuthors

--- 06ApiCrud/main.js
+++ 06ApiCrud/main.js
@@ -1,1 +1,7 @@
 //* EJECUTAR MIS BLOQUES DE CÓDIGO
+//* MAIN ES NUESTRO ARCHIVO JS PRINCIPAL
+
+const goodReadsCrud = require('./crudAuthors')
+
+//*LISTA DE TODOS LOS AUTORES
+goodReadsCrud.listAuthors()
